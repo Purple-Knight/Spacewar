c6131a5bece0a56299411abbe55b0363722e69ef
Esteban <esteban78570@gmail.com>
2020-11-18 18:24:56 +0100

    Update Guy.cpp

--- GarbageCollector/Guy.cpp
+++ GarbageCollector/Guy.cpp
@@ -1,5 +1,9 @@
 #include "Guy.h"
 
+//sf::vetor2f vecteur(3.f, 2.f);
+//float norme = std::sqrt(vecteur.x * vecteur.x + vecteur.y * vecteur.y);
+//vecteur = vecteur / norme;
+
 Guy::Guy(float x_, float y_, sf::RenderWindow* window_, Player* player_) : Enemy(x_, y_, window_)
 {
 	player = player_;
@@ -11,9 +15,9 @@ Guy::Guy(float x_, float y_, sf::RenderWindow* window_, Player* player_) : Enemy
 	shape.setPosition(x, y);
 
 	//set direction
-	//direction.x = player->playerShape.getPosition().x - x;
-	//direction.y = player->playerShape.getPosition().y - y;
-	
+	direction.x = player->playerShape.getPosition().x - x;
+	direction.y = player->playerShape.getPosition().y - y;
+	direction = direction / std::sqrt(direction.x * direction.x + direction.y * direction.y);
 
 	//debug
 	std::cout << "Guy : " << shape.getPosition().x << ", " << shape.getPosition().y << std::endl;
@@ -28,11 +32,14 @@ void Guy::Update(float deltaTime)
 void Guy::UpdateMove(float deltaTime)
 {
 	Enemy::UpdateMove(deltaTime);
-	//direction.x = player->playerShape.getPosition().x - x;
-	//direction.y = player->playerShape.getPosition().y - y;
-	//x += direction.x * speed * deltaTime;
-	//y += direction.y * speed * deltaTime;
-	//shape.setPosition(x, y);
+	direction.x = player->playerShape.getPosition().x - x;
+	direction.y = player->playerShape.getPosition().y - y;
+	
+	direction = direction / std::sqrt(direction.x * direction.x + direction.y * direction.y);
+	
+	x += direction.x * speed * deltaTime;
+	y += direction.y * speed * deltaTime;
+	shape.setPosition(x, y);
 }
 
 Guy::~Guy()
